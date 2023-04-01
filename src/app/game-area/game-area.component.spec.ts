bf64e6bd2e90e5cbed989cde203981b223309238
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-04-01 15:16:47 -0500

    fixed spacing in player card deck area and other minor css cleanup and adjustments

--- src/app/game-area/game-area.component.spec.ts
+++ src/app/game-area/game-area.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { OpponentHandComponent } from './opponent-hand.component';
+import { GameAreaComponent } from './game-area.component';
 
-describe('OpponentHandComponent', () => {
-  let component: OpponentHandComponent;
-  let fixture: ComponentFixture<OpponentHandComponent>;
+describe('GameAreaComponent', () => {
+  let component: GameAreaComponent;
+  let fixture: ComponentFixture<GameAreaComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ OpponentHandComponent ]
+      declarations: [ GameAreaComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(OpponentHandComponent);
+    fixture = TestBed.createComponent(GameAreaComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
